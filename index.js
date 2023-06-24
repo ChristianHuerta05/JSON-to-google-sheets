8f5f6735f706023cf85212c458f8ca057ca1572f
zippiertomcat <christianhuerta309@gmail.com>
2023-06-23 18:03:25 -0700

    code added

---
+++ index.js
@@ -0,0 +1,55 @@
+const { google } = require('googleapis');
+const credentials = require('path-to-key-file.json');
+const fs = require('fs');
+const jsonData = require('path-to-JSON-data.json')
+
+const spreadsheetId = 'google-sheet-id';
+const sheetName = 'sheet-name';
+
+
+
+
+// Function to insert JSON data into Google Sheets and replace the existing data
+async function insertJSONIntoSheet() {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    });
+    const client = await auth.getClient();
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    // get the reviews from the JSON data
+    const reviews = jsonData.reviews;
+
+    
+    const columnNames = ['Name', 'Location', 'Rating', 'Text', 'Date'];//only for first entry, if columns already named remove
+
+   //if columns already named remove columnNames. Gets data from JSON
+    const valuesWithColumnNames = [columnNames, ...reviews.map(review => [
+      review.name,
+      review.location,
+      review.rating,
+      review.text,
+      review.date
+    ])];
+
+
+    // Insert the JSON data
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: `${sheetName}!A1:E`,
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: valuesWithColumnNames
+      }
+    });
+
+
+  } catch (error) {
+    console.error('Error inserting data:', error);
+  }
+}
+
+
+insertJSONIntoSheet();
